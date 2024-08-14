c6158fe46b25e240b3570015904edfc2af3027ce
Harvey Que <Q-Query@outlook.com>
2024-08-15 00:04:53 +0800

    bugfix

--- protium/cli.py
+++ protium/cli.py
@@ -3,9 +3,9 @@
 import json
 
 import click
-from api import ApiClient
+from protium.api import ApiClient
 
-from ..version import __version__
+from version import __version__
 
 # 创建 ApiClient 实例
 api_client = ApiClient()
